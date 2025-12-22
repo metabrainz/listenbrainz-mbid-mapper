296b9895792572f2202e777a29e0a63e6d7b9f78
Robert Kaye <rob@musicbrainz.org>
2025-12-22 11:46:20 +0100

    add cache items to stats

--- mapper/statistics.hpp
+++ mapper/statistics.hpp
@@ -9,15 +9,21 @@ class Statistics {
         unsigned long num_200_requests;
         unsigned long num_400_requests;
         unsigned long num_404_requests;
+        unsigned long cache_items;
 
     public:
-        Statistics() : num_200_requests(0), num_400_requests(0), num_404_requests(0) {}
+        Statistics() : num_200_requests(0), num_400_requests(0), num_404_requests(0), cache_items(0) {}
 
         void update(int delta_200, int delta_400, int delta_404) {
             std::lock_guard<std::mutex> lock(mtx);
             num_200_requests += delta_200;
             num_400_requests += delta_400;
             num_404_requests += delta_404;
+        }
+       
+        // CALL THIS!
+        void update_cache_items(unsigned long _cache_items) {
+            cache_items = _cache_items;
         }
 
         unsigned long get_200_count() {
@@ -49,6 +55,9 @@ class Statistics {
             data += string("# HELP lbmapper_rss The current resident set size, measured in megabytes.\n");
             data += string("# TYPE lbmapper_rss gauge\n");
             data += string("lbmapper_rss ") + to_string(rss_size) + "\n";
+            data += string("# HELP lbmapper_cache_items The current number of items in index cache.\n");
+            data += string("# TYPE lbmapper_cache_items gauge\n");
+            data += string("lbmapper_cache_items ") + to_string(cache_items) + "\n";
             
             return data;
         }
